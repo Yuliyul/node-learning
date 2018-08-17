a6090c2ec3bc4bf4355bfbf0668a1acbd4cbd760
Yuliyul <ykhazran@gmail.com>
2018-08-17 12:59:54 +0300

    'Initial'

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('curyear', ()=>{ return new Date().getFullYear()});
+hbs.registerHelper('cupitalize', (text)=>{ return text.toUpperCase()});
+app.set('view engine', 'hbs'); 
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next)=>{
+	var now = new Date().toString();
+	var log = `now is ${now} and ${req.method} and ${req.url}` + '\n';
+	console.log(log);
+	fs.appendFile('log.log', log, (err)=>{
+		if(err)
+			console.log('Some error' + err)});
+	next();  
+});
+app.use((req, res, next)=>{
+	res.render('maintain.hbs',
+	{
+		pageTitle:"maintainance"
+	}); 
+});
+app.get('/', (req, resp)=>{
+	// resp.send('<h1>Hello world</h1>');
+	resp.render('home.hbs',
+		{
+			pageTitle:"Start page",
+			name:'Yul Kh'
+		});
+});
+app.get('/about', (req, resp)=>{
+	resp.render('about.hbs',
+		{
+			pageTitle:"About page title"
+		});
+	
+});
+app.get('/bad', (req, resp)=>{
+	resp.send(
+		{
+			errorMessage:'Not good'
+		});
+});
+app.listen(3000,()=>{console.log('Server start')});
